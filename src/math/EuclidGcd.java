e4057eb396ef68ad9f3c44ee629f0551dcf52293
Ankit Pandoh <pandoh.2007@gmail.com>
2018-10-08 15:50:13 +0530

    GCD for number 1180 and 482

--- src/math/EuclidGcd.java
+++ src/math/EuclidGcd.java
@@ -6,7 +6,7 @@ package math;
  */
 public class EuclidGcd {
 	public static void main(String[] args){
-		int a = 16, b = 10;
+		int a = 1180, b = 482;
 		while(b != 0){
 			int rem = a%b;
 			a = b;
